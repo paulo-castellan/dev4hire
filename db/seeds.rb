cacb4983f6d26c0d9693cb1f78b3b16033d18946
paulo-castellan <paulo.castellan@gmail.com>
2021-10-14 09:50:07 -0300

    show added to projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+dev = Dev.create!(email: 'dev@dev.com', password: 123456)
+expertise = Expertise.create!(field_of_expertise: 'Dev Back-end')
+dev_profile = DevProfile.create!(full_name: 'João Carlos', social_name: 'João Carlos',
+                                date_of_birth: '25/11/1987', academic_education: 'Formado em SI pela UFPE',
+                                previous_experience: 'Estágio com backend na XX consult',
+                                dev: dev, expertise: expertise, description: 'Desenvolvedor que curte a vida')
+work_type = WorkType.create!(description: 'Remoto')
+user = User.create!(email: 'user@user.com', password: 123456)
+Project.create!(title: 'Aplicação web Front/Back-end', detailed_description: 'Nesse projeto equipes vão estar envolvidas na construção de uma aplicação web para uma grande empresa de mineração',
+                          skill_sets: 'Ruby on Rails', hire_date_limit: '10/12/2021',
+                          max_payment_per_hour: '200', work_type: work_type, user: user)
+Project.create!(title: 'Suporte Back-end', detailed_description: 'Esse projeto necessita de uma grande quantidade de dev back-end para expansão de aplicação web',
+                            skill_sets: 'Laravel/Node.js', hire_date_limit: 3.days.from_now,
+                            max_payment_per_hour: '500', work_type: work_type, user: user)
+Project.create!(title: 'Front-end Needed', detailed_description: 'Estamos procurando por Devs freelancers para realizarem a criação de telas temáticas URGENTE!',
+                              skill_sets: 'React JS/Vue JS', hire_date_limit: 3.days.from_now,
+                              max_payment_per_hour: '150', work_type: work_type, user: user)
--- spec/system/dev_relates_to_project_spec.rb
+++ spec/system/dev_relates_to_project_spec.rb
@@ -24,6 +24,33 @@ describe 'Dev' do
     expect(page).to have_content('R$ 200,00')
   end
 
+  it 'clicks on a specific project to see details' do
+    jonas = Dev.create!(email: 'dev@dev.com', password: 123456)
+    expertise = Expertise.create!(field_of_expertise: 'Dev Back-end')
+    dev_profile = DevProfile.create!(full_name: 'João Carlos', social_name: 'João Carlos',
+                                    date_of_birth: '25/11/1987', academic_education: 'Formado em SI pela UFPE',
+                                    previous_experience: 'Estágio com backend na XX consult',
+                                    dev: jonas, expertise: expertise, description: 'Desenvolvedor que curte a vida')
+    user = User.create!(email: 'user@user.com', password: 123456)
+    work_type = WorkType.create!(description: 'Remoto')
+    project = Project.create!(title: 'Aplicação web Front/Back', detailed_description: 'Nesse projeto as equipes vão estar envolvidas na construção de uma aplicação web para uma grande empresa de mineração',
+                              skill_sets: 'Ruby on Rails', hire_date_limit: '10/12/2021',
+                              max_payment_per_hour: '200', work_type: work_type, user: user)
+
+    login_as jonas, scope: :dev
+    visit root_path
+    click_on 'Área de projetos'
+    click_on 'Aplicação web Front/Back'
+
+    expect(page).to have_content('Aplicação web Front/Back')
+    expect(page).to have_content('Nesse projeto as equipes vão estar envolvidas na construção de uma aplicação web para uma grande empresa de mineração')
+    expect(page).to have_content('Ruby on Rails')
+    expect(page).to have_content('10/12/2021')
+    expect(page).to have_content('R$ 200,00')
+    expect(page).to have_content('Remoto')
+    expect(page).to have_content('Gerente do projeto: user@user.com')
+  end
+
   it 'cannot find project that have hire date limit before atual date' do
     dev = Dev.create!(email: 'dev@dev.com', password: 123456)
     expertise = Expertise.create!(field_of_expertise: 'Dev Back-end')
